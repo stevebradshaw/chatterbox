069643f0579b2e882e67ddc8c567ae085d2d7fdb
Steve Bradshaw <sjb1970@gmail.com>
2015-05-18 06:05:12 +0100

    Add javascript to setup UI (WIP)

--- public/js/chatterbox.js
+++ public/js/chatterbox.js
@@ -1,8 +1,44 @@
-$(document).ready(function() {
+function populateRoomList() {
 
-	console.log('document ready') ;
+/*
+<a id="selectRoom" href="#" class="list-group-item">Lobby<span class="badge">27</span></a>
+
+              a(href="#", class="list-group-item") Lobby
+                span.badge 27
+              a(href="#", class="list-group-item active") Snug
+                span.badge 6
+              a(href="#", class="list-group-item") Private 
+                span.badge 2
+*/
+	$("#room-select-group").append('<span id="select-room" class="list-group-item">Bar<span class="badge">13</span></span>') ;
+	$("#room-select-group").append('<a id="select-room" href="#" class="list-group-item">Lobby<span class="badge">27</span></a>') ;
+	$("#room-select-group").append('<a id="select-room" href="#" class="list-group-item">Snug<span class="badge">4</span></a>') ;
+}
+
+function populateRoomOccupants() {
+/*	a(href="#", class="list-group-item") Tom
+    a(href="#", class="list-group-item") Dick
+    a(href="#", class="list-group-item") Harry */
+
+}
+function setupButtons() {
 
 	$("#btn-new-room").click(function() {
 		console.log('create room') ;
 	}) ;
+
+	$("#select-room").click(function() {
+		console.log('select room') ;
+	}) ;
+}
+
+
+$(document).ready(function() {
+
+	console.log('document ready') ;
+
+	populateRoomList() ;
+
+	setupButtons() ;
+
 }) ;
