2c25f3dcd9c9c6530f0232c46dbc7d2ecb5cff90
Steve Bradshaw <sjb1970@gmail.com>
2015-05-27 06:37:52 +0100

    Tidy up source (WIP)

---
+++ modules/occupants.js
@@ -0,0 +1,6 @@
+var roomOccupants = [{"room": "Auditorium", "occupants": [], "ids": []},
+                     {"room": "Bar", "occupants": [], "ids": []},
+                     {"room": "Lobby", "occupants": [], "ids": []},
+                     {"room": "Snug", "occupants": [], "ids": []}] ;
+
+                     
